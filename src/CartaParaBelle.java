9bf95bf8531560d0a0efb2bff844762acbba9572
Chris <christoffer.rodrigues@escola.pr.gov.br>
2024-06-02 16:36:00 -0300

    Implementações

--- src/CartaParaBelle.java
+++ src/CartaParaBelle.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
+import java.time.LocalDate;
 
 public class CartaParaBelle {
 
     public static void main(String[] args) {
-        System.out.println("Não é muito, mas foi feito com amor");
-        System.out.println("Nossas Memórias");
+        System.out.println("Não é muito, mas este projeto foi feito com  muito amor...");
+        System.out.println("Dedico esta carta para Isabelle Belizotti, minha namorada e eterna companheira");
 
-
-        //Data do início do nosso relacionamento
-
-        int dia = 21;
-        int mes = 3;
-        int ano  = 2023;
 
         //Dados incluídos na carta
 
         String Autor;
-        Autor = "Amor da sua vida";
+        Autor = "Christoffer Isac, o seu eterno companheiro";
 
-        /* Carta MinhaCarta;
-        Carta = MinhaCarta = new Carta(); */
+        LocalDate DataCarta = LocalDate.of(2024, 6, 2);
+        System.out.println("última atualização: " + DataCarta);
 
-
-        System.out.println("Tudo começou aqui: " + dia + "/" + mes + "/" +  ano);
+        LocalDate DataRelacionamento = LocalDate.of(2023, 3, 21);
 
         String CartaDeAmor;
         CartaDeAmor = """ 
@@ -49,7 +43,19 @@ public class CartaParaBelle {
                 já estavámos juntos, especificamente dia 21/03/2023, quando me tornei o seu namorado e quando
                 você viria a ser muito mais do que somente a minha namorada...""";
 
+
             System.out.println(CartaDeAmor);
+
+            System.out.println(""" 
+                    
+                    Tudo começou aqui:""" +  DataRelacionamento);
+
+            System.out.println("""
+                    
+                    Foi quando nasceu o nosso amor...""");
+
+            String Memorias = "Quer se aprofundar ainda mais na história desse relacionamento?";
+            System.out.println(Memorias);
 
 
         } else if (resposta.equalsIgnoreCase("Não")) {
@@ -64,9 +70,57 @@ public class CartaParaBelle {
 
 
         } else {
-            System.out.println("Amor, para prosseguirmos você digitar sim ou não");
+            System.out.println("Amor, para prosseguirmos você pode digitar sim ou não");
             // Adicione aqui o código que deve ser executado em caso de resposta inválida
         }
+
+        Scanner scanner1 = new Scanner(System.in);
+        String resposta1 = scanner.nextLine();
+
+
+        if (resposta.equalsIgnoreCase("Sim")) {
+            System.out.println("E foi...");
+
+            CartaDeAmor = """ 
+               
+               Desde aquele instante iniciamos essa luta pela nossa relação, tendo que conviver com
+               a distância estabelecida entre nós dois. Além das grandes dificuldades de aceitação dos seus pais,
+               que continuam existindo até os dias atuais. Mas mesmo com tudo isto que tentou nos derrubar e
+               impedir o prosseguimento da nossa relação, você não desistiu e igualmente eu jamais desistirei.
+               Assim fomos para o nosso segundo encontro...""";
+
+            System.out.println(CartaDeAmor);
+
+            CartaDeAmor = """ 
+               
+               Que a princípio demorou quase 6 meses para conseguirmos esta oportunidade, mas que felizmente aconteceu.
+               Foi quando nossa relação de fato saiu das telas, foi quando nosso amor pôde ser concretizado através dos
+               nossos pequenos gestos... Foi naquele instante que eu tive a certeza de que seria para sempre, não somente
+               uma fase, mas sim uma vida. Mas ainda tinhámos que aprender muitas coisas...""";
+
+            System.out.println(CartaDeAmor);
+
+            String Memorias1 = "Posso contar um pouco mais?";
+            System.out.println(Memorias1);
+
+
+        } else if (resposta.equalsIgnoreCase("Não")) {
+            System.out.println("Encerrando a execução do código.");
+
+            CartaDeAmor = """ 
+                
+                Respeito sua descisão... Se esta é sua resposta imagino que possa estar em um momento ruim amor,
+                porém independente de como esteja, eu sempre estarei contigo. Igualmente Deus sempre lhe guardará.""";
+
+            System.out.println(CartaDeAmor);
+
+
+        } else {
+            System.out.println("Amor, para prosseguirmos você pode digitar sim ou não");
+
+        }
+
+
 
         scanner.close();
     }
